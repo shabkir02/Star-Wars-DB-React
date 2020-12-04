bd17d8cd974782dcfb833a74817e0b3c4bc2fd9a
Kirill <shabkir02@gmail.com>
2020-12-04 22:44:19 +0600

    random-planet

---
+++ src/components/error-message/error-message.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './error-message.css';
+import icon from './death-star.png';
+
+const ErrorMessage = () => {
+  return (
+    <div className="error-indicator">
+      <img src={icon} alt="error icon"/>
+      <span className="boom">BOOM!</span>
+      <span>
+        something has gone terribly wrong
+      </span>
+      <span>
+        (but we already sent droids to fix it)
+      </span>
+    </div>
+  );
+};
+
+export default ErrorMessage;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-0k7tw210fb6">
+            <div className="ldio-16bw6hgkx8u">
+                <div></div>
+                <div></div>
+                <div><div></div></div>
+                <div><div></div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
